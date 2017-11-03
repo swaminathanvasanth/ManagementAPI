5e2333c42f6ab7825261f49d9ed0c5dfcce22dd0
Vasanth Rajaraman <vasanth.rajaraman@cps.iisc.ernet.in>
2017-11-03 16:09:46 +0530

    Initial Commit - Registration API

---
+++ src/rbccps/smartcity/registerapi/RequestService.java
@@ -0,0 +1,5 @@
+package rbccps.smartcity.registerapi;
+
+public class RequestService {
+
+}
---
+++ src/rbccps/smartcity/registerapi/ldap/LDAPQuery.java
@@ -0,0 +1,14 @@
+package rbccps.smartcity.registerapi.ldap;
+
+public class LDAPQuery {
+
+	
+	public static int checkProviderLimit(String ProviderID){
+		
+		// Call the LDAP with providerID and find the available OnBoarding limit.
+		
+				
+		return 1;
+	}
+	
+}
